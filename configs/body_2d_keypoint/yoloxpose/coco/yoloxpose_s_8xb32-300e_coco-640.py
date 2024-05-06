018695b24d0a102eb546490db5680831efcce652
imllt <a873570845@gmail.com>
2024-05-07 01:16:40 +0800

    fix

--- configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_s_8xb32-300e_coco-640.py
+++ configs/body_2d_keypoint/yoloxpose/coco/yoloxpose_s_8xb32-300e_coco-640.py
@@ -281,7 +281,7 @@ test_dataloader = val_dataloader
 # evaluators
 val_evaluator = dict(
     type='CocoMetric',
-    ann_file=data_root + 'coco/annotations/person_keypoints_val2017.json',
+    ann_file=data_root + 'val/file.json',
     score_mode='bbox',
     nms_mode='none',
 )
