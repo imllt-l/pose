7e7826a4a342c6ac9a8616e729da9a5aa5b99acc
imllt <a873570845@gmail.com>
2024-05-08 16:55:23 +0800

    fix

--- configs/_base_/datasets/cow-pose.py
+++ configs/_base_/datasets/cow-pose.py
@@ -122,44 +122,28 @@ dataset_info = dict(
             swap=''),
     },
     skeleton_info={
-        0:
-        dict(link=('Neck'), id=0, color=[0, 255, 0]),
-        1:
-        dict(link=('Head_top','Spine'), id=1, color=[0, 255, 0]),
-        2:
-        dict(link=('Neck','Coccyx','Left_FTR','Right_FTR'), id=2, color=[0, 255, 0]),
-        3:
-        dict(link=('Spine','Right_FK'), id=3, color=[0, 255, 0]),
-        4:
-        dict(link=('Right_FTR','Right_FH'), id=4, color=[0, 255, 0]),
-        5:
-        dict(link=('Right_FK'), id=5, color=[0, 255, 0]),
-        6:
-        dict(link=('Spine','Left_FK'), id=6, color=[0, 255, 0]),
-        7:
-        dict(link=('Left_FH','Left_FTR'), id=7, color=[0, 255, 0]),
-        8:
-        dict(link=('Left_FK'), id=8, color=[0, 255, 0]),
-        9:
-        dict(link=('Spine','Right_HT','Left_HTR'), id=9, color=[0, 255, 0]),
-        10:
-        dict(link=('Coccyx','Right_HK'), id=10, color=[0, 255, 0]),
-        11:
-        dict(link=('Right_HT','Right_HH'), id=11, color=[0, 255, 0]),
-        12:
-        dict(link=('Right_HK'), id=12, color=[0, 255, 0]),
-        13:
-        dict(link=('Coccyx','Left_HK'), id=13, color=[0, 255, 0]),
-        14:
-        dict(link=('Left_HTR','Left_HH'), id=14, color=[0, 255, 0]),
-        15:
-        dict(link=('Left_HK'), id=15, color=[0, 255, 0]),
+        0:dict(link=('Neck'), id=0, color=[0, 255, 0]),
+        1:dict(link=('Head_top','Spine'), id=1, color=[0, 255, 0]),
+        2:dict(link=('Neck','Coccyx','Left_FTR','Right_FTR'), id=2, color=[0, 255, 0]),
+        3:dict(link=('Spine','Right_FK'), id=3, color=[0, 255, 0]),
+        4:dict(link=('Right_FTR','Right_FH'), id=4, color=[0, 255, 0]),
+        5:dict(link=('Right_FK'), id=5, color=[0, 255, 0]),
+        6:dict(link=('Spine','Left_FK'), id=6, color=[0, 255, 0]),
+        7:dict(link=('Left_FH','Left_FTR'), id=7, color=[0, 255, 0]),
+        8:dict(link=('Left_FK'), id=8, color=[0, 255, 0]),
+        9:dict(link=('Spine','Right_HT','Left_HTR'), id=9, color=[0, 255, 0]),
+        10:dict(link=('Coccyx','Right_HK'), id=10, color=[0, 255, 0]),
+        11:dict(link=('Right_HT','Right_HH'), id=11, color=[0, 255, 0]),
+        12:dict(link=('Right_HK'), id=12, color=[0, 255, 0]),
+        13:dict(link=('Coccyx','Left_HK'), id=13, color=[0, 255, 0]),
+        14:dict(link=('Left_HTR','Left_HH'), id=14, color=[0, 255, 0]),
+        15:dict(link=('Left_HK'), id=15, color=[0, 255, 0]),
     },
     joint_weights=[
-        1., 1., 1., 1.2, 1.2, 1.2, 1.2, 1.2, 1.2, 1., 1.5, 1.2, 1.2, 1.2, 1.2, 1.2
+        1., 1., 1., 1.2, 1.2, 1.2, 1.2, 1.2, 1., 1., 1.5, 1.2, 1., 1.2, 1.2, 1.2,0.01
     ],
     sigmas=[
         0.026, 0.025, 0.025, 0.035, 0.035, 0.079, 0.079, 0.072, 0.072, 0.062,
-        0.062, 0.107, 0.107, 0.087, 0.087, 0.089
+        0.062, 0.107, 0.107, 0.087, 0.087, 0.089,0.001
     ]
 )
